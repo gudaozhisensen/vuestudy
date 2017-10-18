a7815772ae2cd1a679a3a2e46827cc88549dd1fd
gudaozhisensen <593866511@qq.com>
2017-10-18 18:06:43 +0800

    基于vue的表格
    基于vue的表格

---
+++ severs.php
@@ -0,0 +1,14 @@
+<?php
+header("content-type:text/plain;charset=utf-8")
+
+
+//定义一个多维数组
+$staff=array(
+array("name"=>"洪七","number"=>"01","sex"=>"男","job"=>"总经理"),
+array("name"=>"郭靖","number"=>"02","sex"=>"男","job"=>"开发工程师"),
+array("name"=>"郭靖","number"=>"02","sex"=>"男","job"=>"开发工程师"),
+);
+
+
+
+?>
